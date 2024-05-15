b6fd16b11f23c8a3f05d51f0a2688a56e3893d43
jainab11 <shaikhjainab11@gmail.com>
2024-05-15 11:19:11 +0530

    [Jainab] Add: paython script for snake and ladder game

---
+++ snake_and_ladder.py
@@ -0,0 +1,15 @@
+# import random
+def dice_roll():
+    # return random.randint(1,6)
+def player_position():
+    pass
+def play_game():
+    pass
+def main():
+    start_pos =  1
+    last_pos = 100
+    dice_roll = 0
+    pass
+if __name__==  "__main__":
+    main()
+    
